9934f3d8670a0f717a639b178612b95925cd5595
Lana Mustafić <lana.mustafic@edu.fit.ba>
2026-03-27 17:51:06 +0100

    Added migration for Fluent API updates

--- src/NuaSpa.Infrastructure/Migrations/20260327165042_Task3_2_FluentApi_Update.Designer.cs
+++ src/NuaSpa.Infrastructure/Migrations/20260327165042_Task3_2_FluentApi_Update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NuaSpa.Infrastructure;
 
@@ -11,9 +12,11 @@ using NuaSpa.Infrastructure;
 namespace NuaSpa.Infrastructure.Migrations
 {
     [DbContext(typeof(NuaSpaContext))]
-    partial class NuaSpaContextModelSnapshot : ModelSnapshot
+    [Migration("20260327165042_Task3_2_FluentApi_Update")]
+    partial class Task3_2_FluentApi_Update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
